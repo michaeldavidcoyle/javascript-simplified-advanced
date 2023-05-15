0024400633398ec08edb6d944250055971bd7f60
Michael Coyle <michael.david.coyle4@gmail.com>
2023-05-15 16:21:22 -0500

    add constructor and clear method

--- calculator/Calculator.js
+++ calculator/Calculator.js
@@ -1,1 +1,19 @@
-export default class Calculator {}
+export default class Calculator {
+    constructor(
+        primaryOperandDisplay,
+        secondaryOperandDisplay,
+        operationDisplay
+    ) {
+        this.primaryOperandDisplay = primaryOperandDisplay;
+        this.secondaryOperandDisplay = secondaryOperandDisplay;
+        this.operationDisplay = operationDisplay;
+
+        this.clear();
+    }
+
+    clear() {
+        this.primaryOperandDisplay.textContent = 0;
+        this.secondaryOperandDisplay.textContent = '';
+        this.operationDisplay.textContent = '';
+    }
+}
